151386d511a55051f015db6d813e53440abbba83
alsfkl222 <alskfl222@kakao.com>
2022-07-01 09:47:54 +0900

    FIX: cors

--- image-server/app.js
+++ image-server/app.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from 'cors'
 
 const app = express()
 const port = 5005
 
+app.use(cors())
 app.use('/static', express.static('static'))
 
 app.get('/', (req, res) => {
